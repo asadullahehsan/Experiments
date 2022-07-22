c9d2906aa8e1757a8b9b945b6397157e10a9b6b2
asadullahehsan <asadullahehsan10@gmail.com>
2022-07-22 22:54:31 +0500

    Convert JSON Array String to List
    Convert JSON Array String to List in C#

--- Experiments/Program.cs
+++ Experiments/Program.cs
@@ -1,6 +1,15 @@
 ﻿using Experiments;
 using System.Text.Json;
 
-string str = "{\"Id\": 101, \"Name\": \"IT\"}";
- Department department = JsonSerializer.Deserialize<Department>(str)!;
+string jsonArray = "{\"Id\": 101, \"Name\": \"IT\"}";
+Department department = JsonSerializer.Deserialize<Department>(jsonArray)!;
 Console.WriteLine($"{department.Id}: {department.Name}");
+
+//string jsonArray = "[{\"Id\": 101,\"Name\":\"IT\" }, {\"Id\": 102,\"Name\":\"Accounts\" }]";
+//var deptList = JsonSerializer.Deserialize<IList<Department>>(jsonArray);
+
+//foreach (var dept in deptList!)
+//{
+//    Console.WriteLine("Department Id is: {0}", dept.Id);
+//    Console.WriteLine("Department Name is: {0}", dept.Name);
+//}
