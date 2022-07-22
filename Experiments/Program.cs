55fc5b77fff10558408fa8972a0aea7c10d20b72
asadullahehsan <asadullahehsan10@gmail.com>
2022-07-22 22:39:08 +0500

    Add project files.

---
+++ Experiments/Program.cs
@@ -0,0 +1,6 @@
+﻿using Experiments;
+using System.Text.Json;
+
+string str = "{\"Id\": 101, \"Name\": \"IT\"}";
+ Department department = JsonSerializer.Deserialize<Department>(str)!;
+Console.WriteLine($"{department.Id}: {department.Name}");
