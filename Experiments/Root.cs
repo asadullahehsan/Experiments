4cb6adc162fa30be4a1eae7272cbb09e6da585e9
asadullahehsan <asadullahehsan10@gmail.com>
2022-07-25 22:57:46 +0500

    converting json string to object
    sms has status and timestamp

---
+++ Experiments/Root.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Experiments
+{
+    public class Root
+    {
+        public Sms sms { get; set; }
+    }
+
+    public class Sms
+    {
+        public string status { get; set; }
+        public string timestamp { get; set; }
+    }
+}
